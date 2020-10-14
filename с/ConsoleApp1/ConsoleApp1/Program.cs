76e35a17597f220ccbe5b34dcf662c326162d20a
SEM301 <72428570+SEM301@users.noreply.github.com>
2020-10-14 21:07:55 +0900

    Add files via upload

---
+++ с/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            try
+            {
+                int num = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"Ваше число-{num}");
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine($"Не верный формат");
+            }
+            finally
+            {
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
